8a8c6825a099fc9fc38a8e687f0086046459d1ba
Yoo Park <74237804+yoondray@users.noreply.github.com>
2021-02-18 14:04:33 +0900

    Update DatabaseConfig.py

--- chatbot/config/DatabaseConfig.py
+++ chatbot/config/DatabaseConfig.py
@@ -1,8 +1,8 @@
 
 >>> DB_HOST="127.0.0.1"
->>> DB_USER="homestead"
+>>> DB_USER="root"
 >>> DB_PASSWORD="1234"
->>> DB_NAME="mysql"
+>>> DB_NAME="study_db"
 >>> 
 >>> def DatabaseConfig():
 	global DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
