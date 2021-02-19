909d43d49dd5ce06d28093855bb43775f7ecb79a
Yoo Park <74237804+yoondray@users.noreply.github.com>
2021-02-19 16:32:40 +0900

    Update create_train_data_table.py

--- chatbot/train_tools/qna/create_train_data_table.py
+++ chatbot/train_tools/qna/create_train_data_table.py
@@ -1,33 +1,30 @@
 import pymysql
-from config.DatabaseConfig import *
+from Scripts.chatbot.config.DatabaseConfig import *
 
 db = None
 try:
-    db = pymysql.connect(
+    db=pymysql.connect(
         host=DB_HOST,
         user=DB_USER,
         passwd=DB_PASSWORD,
         db=DB_NAME,
-        charset='utf-8'
+        charset='utf8'
         )
 
-
-# (backtick```) 활용 - 객체이름을 감쌀 때 
-# (apostrophe''')활용 - 문자열 데이터(날짜, 상수 등) 입력  *except: number, boolean, null  
     sql='''
-        CREATE TABLE IF NOT EXISTS `chatbot_train_data` (
-        `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-        `intent` VARCHAR(45) NULL,
-        `ner` VARCHAR(1024) NULL,
-        `query` TEXT NULL,
-        `answer` TEXT NOT NULL,
-        `answer_image` VARCHAR(2048) NULL,
-        PRIMARY KEY(`id`))
+    CREATE TABLE IF NOT EXISTS `chatbot_train_data` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `intent` VARCHAR(45) NULL,
+    `ner` VARCHAR(1024) NULL,
+    `query` TEXT NULL,
+    `answer` TEXT NOT NULL,
+    `answer_image` VARCHAR(2048) NULL,
+    PRIMARY KEY(`id`))
     ENGINE = InnoDB DEFAULT CHARSET=utf8
     '''
 
     with db.cursor() as cursor:
-        cursor.exevute(sql)
+        cursor.execute(sql)
 
 except Exception as e:
     print(e)
